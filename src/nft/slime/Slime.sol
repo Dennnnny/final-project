57627e6a0e1f9530b103e34b2b64dc37461ba41c
dennnnny <dcno017@gmail.com>
2023-12-22 18:00:05 +0800

    keep constructing project

--- src/nft/slime/Slime.sol
+++ src/nft/slime/Slime.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; // ERC721
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol"; // ReentrancyGuard
+import "src/erc20/Slimetoken.sol"; // self made token
+
 // I think I should make a factory to create erc721
 contract SlimeFactory {
 
